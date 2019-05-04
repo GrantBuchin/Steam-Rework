977f9e3be8a792ff5b7e68be269dae5a3059a15a
GrantBuchin <49158275+GrantBuchin@users.noreply.github.com>
2019-05-04 08:55:32 -0400

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,19 @@
-import React, { Component } from "react";
-import image1 from "./images/image1.jpg.png";
-
+import React, { Component } from 'react';
+import image1 from './images/image1.jpg.png';
+import './Home.css';
 
 class Home extends Component {
-  
   render() {
-	  
-    return ( 
-	
+    return (
       <div>
-        <h2>Steam Recommendations  </h2>
-        <p></p>
-      
-        <p></p>
-        <img class="reframe" src={image1}/>
+        <h2>Steam Recommendations </h2>
+        <p />
+
+        <p />
+        <img className='reframe' src={image1} alt='image1' />
       </div>
     );
   }
 }
-
- 
- 
 
 export default Home;
